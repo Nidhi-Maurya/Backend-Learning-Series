a8b6d6ee193e4d0eb2fbdeb9b915151dee1495a2
Nidhi Maurya <mauryanidhi1610@gmail.com>
2025-06-06 08:19:52 +0530

    new project creation

---
+++ miniProject/joke_generator.js
@@ -0,0 +1,38 @@
+
+// https://api.api-ninjas.com/v1/jokes
+
+
+import https from  'https';
+import chalk from 'chalk';
+
+
+const getJoke=()=>{
+  const url="https://v2.jokeapi.dev/joke/Any";
+   https.get(url,(res)=>{
+    let data="";
+    res.on('data',(chunk)=>{
+      data+=chunk;
+    });
+     res.on('end',()=>{
+       const joke=JSON.parse(data);
+      
+      
+      console.log(`Here is a random ${joke.type} joke:`)
+      console.log(chalk.green(`${joke.setup}`));
+      // 
+    
+
+     })
+
+     res.on('error',(err)=>{
+      console.error(`Error fetching the joke, ${err.message}`)
+     })
+
+   })
+}
+
+
+
+
+
+getJoke();
